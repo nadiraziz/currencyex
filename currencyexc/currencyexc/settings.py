1489ab39b6649cf37e2ee125703bfd41a2ce56a1
Nadir Abdul Aziz <59334616+nadiraziz@users.noreply.github.com>
2022-06-21 20:09:10 +0530

    ready

--- currencyexc/currencyexc/settings.py
+++ currencyexc/currencyexc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)2de1njqiusp^i^^e@yq+*(=r#2f=ta+(@wa)k3q--4$bqp40#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'currency-exc.herokuapp.com']
 
 
 # Application definition
